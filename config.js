794f151c7ffadf37bd273a990b5e1054775e1c36
DarkWinzo <darkwinzo4440@gmail.com>
2023-01-27 14:01:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,38 +5,6 @@
   ██║▄▄ ██║██║   ██║██╔══╝  ██╔══╝  ██║╚██╗██║╚════╝██║╚██╔╝██║██║  ██║   
   ╚██████╔╝╚██████╔╝███████╗███████╗██║ ╚████║      ██║ ╚═╝ ██║██████╔╝   
    ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝      ╚═╝     ╚═╝╚═════╝   
-  
-══════════════════════════════════════════════════════════════════════════
-                            ( OWNER  DETAILS )
-══════════════════════════════════════════════════════════════════════════
-✅ Queen Multi-device Whatsapp Bot
-✅ Coded By DarkWinzo 
-══════════════════════════════════════════════════════════════════════════
-                              ( CONTACT ME )
-══════════════════════════════════════════════════════════════════════════
-✅ Telegram: https://t.me/DarkWinzo
-✅ Whatsapp: https://wa.link/jw9ki6
-✅ Instagram:https://msng.link/o/?darkwinzo=ig
-✅ Youtube: https://youtube.com/c/DarkWinzo                                                                                                      
-══════════════════════════════════════════════════════════════════════════
-                             ( PROJECT DETAILS )
-══════════════════════════════════════════════════════════════════════════
-✅ @project_name Queen-MD [WA Multi-device]
-✅ @author DarkWinzo <https://github.com/DarkWinzo>
-✅ @description A WhatsApp based 3ʳᵈ party application that provide many 
-   services with a real-time automated conversational experience
-✅ @link <https://github.com/DarkWinzo/Queen-MD>
-✅ @version 0.0.1
-══════════════════════════════════════════════════════════════════════════
-                             ( PROJECT RULES )
-══════════════════════════════════════════════════════════════════════════
-✅ If you want to recode, reupload
-   or copy the codes/script,
-   please Contact me;
-✅ If the recoded/reupload or copy script? "i will take action immediately"
-✅ © 2022 Queen Bot Inc.Technical Hacker Team
-✅ God Bless You, Family and Myself
-══════════════════════════════════════════════════════════════════════════
 */
 
 const fs = require('fs')
@@ -53,7 +21,7 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'cUZVQXFxaTU=',
+global.session = process.env.SESSION_ID || 'PUT HERE',
 global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
 let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
